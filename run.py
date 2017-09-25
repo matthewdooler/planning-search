c7e7b216178e44c67ff7c9291ce24fe47987bacb
MatthewDooler <MatthewDooler@users.noreply.github.com>
2017-09-25 20:44:18 +0100

    First commit

---
+++ run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import requests
+from time import sleep
+
+startNumber = 67262 # TODO: get this from the highest number (+1) in data directory
+
+i = startNumber
+while True:
+	filename = str(i) + ".pdf"
+	uri = 'https://services.salford.gov.uk/planning/' + filename
+	r = requests.get(uri, stream=True)
+	if r.status_code == 200:
+		with open('data/' + filename, 'wb') as f:
+			for block in r.iter_content(1024):
+				f.write(block)
+		print("Saved " + filename)
+	elif r.status_code == 404:
+		print("Error " + str(r.status_code) + " for " + filename)
+	else:
+		print("Error " + str(r.status_code) + " for " + filename)
+		r.raise_for_status()
+	i = i + 1
+	sleep(0.2)
+
